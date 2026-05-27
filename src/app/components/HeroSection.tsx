8280133a4b7afb6f5adeca7970058457e00de8d1
BitGladiator <karansh1232@gmail.com>
2026-05-27 15:41:16 +0530

    Added the resume

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -92,7 +92,11 @@ function Herosection() {
         </div>
 
         <div className="mt-10">
-          <Link href={"/projects"}>
+          <Link
+            href="/resume/Karan-Sharma-Resume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <Button
               borderRadius="1.75rem"
               className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
